b6c3934f33a07373d6d0ff8df8dcb4899a9de925
Daniel Curtin <dcurtin.work@gmail.com>
2023-01-03 15:32:53 -0700

    Add checking for lock clicking and locking functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 class Color{
-    constructor(hex){
-        this.hex = hex
+    constructor(){
+        this.hex = ""
         this.locked = false
     } 
 }
@@ -8,13 +8,13 @@ class Color{
 
 
 class Palette{
-    constructor(a, b, c, d, e){
+    constructor(){
         this.colors = [
-            new Color(a),
-            new Color(b),
-            new Color(c),
-            new Color(d),
-            new Color(e)
+            new Color(),
+            new Color(),
+            new Color(),
+            new Color(),
+            new Color()
         ]
         this.id = Date.now()
     }
@@ -26,10 +26,21 @@ class Palette{
             }
         }
     }
+
+    lockColor(id) { "lock-3"
+        var element = document.querySelector(`#${id}`)
+        var strInt = id.substr(id.length - 1, 1)
+        var num = parseInt(strInt)
+        
+        if (this.colors[num].locked) {
+            this.colors[num].locked = false;
+            element.src = "./icons/black-unlock.png"
+        } else {
+            this.colors[num].locked = true;
+            element.src = "./icons/black-lock.png"
+        }
+    }
 }
-
-
-
 
 
 //buttons
@@ -52,10 +63,19 @@ var hex4 = document.querySelector("#hex-4")
 var hex5 = document.querySelector("#hex-5")
 
 //instances
-var color = new Color()
 var palette = new Palette()
 
 var hexCharacters = ["A", "B", "C", "D", "E", "F", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+color1.addEventListener('click', checkLock)
+color2.addEventListener('click', checkLock)
+color3.addEventListener('click', checkLock)
+color4.addEventListener('click', checkLock)
+color5.addEventListener('click', checkLock)
+
+
+
 
 function getRandomNumber(){
     return Math.floor(Math.random() * hexCharacters.length)
@@ -67,6 +87,12 @@ function randomHexCode(){
         currentHexCode += hexCharacters[getRandomNumber()]
     }
     return `#${currentHexCode}`
+}
+
+function checkLock(event) {
+    if (event.target.className === "lock") {
+        palette.lockColor(event.target.id);
+    }
 }
 
 palette.buildNewPalette()
